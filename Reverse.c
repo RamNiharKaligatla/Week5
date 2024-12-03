5bdb9523ab700517aa1d6510f5b35a5c2082dba3
Ram Nihar Kaligatla <bhavyagunaram@gmail.com>
2024-12-04 00:36:50 +0530

    Update Reverse.c

--- Reverse.c
+++ Reverse.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
+
 int main()
 {
-    int n,i,k;
-    printf("Size: ");
-    scanf("%d", &n);
-    int a[n];
-    printf("Elements: \n");
-    for(i=0;i<n;i++)
+	int n, i, k;
+
+	printf("Size: ");
+	scanf("%d", &n);
+
+	int a[n];
+	printf("Elements: \n");
+	for (i = 0; i < n; i++)
 	{
-        scanf("%d",&a[i]);
-    }
-	for(i=0;i<n/2;i++)
-	{
-		k=a[i];
-		a[i]=a[n-i-1];
-		a[n-i-1]=k;
+		scanf("%d", &a[i]);
 	}
-	for(i=0;i<n;i++)
+
+	for (i = 0; i < n / 2; i++)
 	{
-		printf("%d ",a[i]);
+		k = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = k;
+	}
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
 	}
 	return 0;
 }
-
