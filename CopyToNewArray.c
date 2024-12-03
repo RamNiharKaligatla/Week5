ebd9c5054904884bd7d35ec6f7a217165ed1093b
Ram Nihar Kaligatla <bhavyagunaram@gmail.com>
2024-12-03 22:41:31 +0530

    a

--- CopyToNewArray.c
+++ CopyToNewArray.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 int main()
 {
-	int n,i;
+	int n, i;
 	printf("Size of array: ");
-	scanf("%d",&n);
+	scanf("%d", &n);
 	int a[n];
- 	printf("Elements:\n");
-	for(i=0;i<n;i++)
+	printf("Elements:\n");
+	for (i = 0; i < n; i++)
 	{
-		scanf("%d",&a[i]);
+		scanf("%d", &a[i]);
 	}
 	int b[n];
-	for(i=0;i<n;i++)
+	for (i = 0; i < n; i++)
 	{
-		b[i]=a[i]
+		b[i] = a[i];
 	}
 	return 0;
 }
--- Frequency.c
+++ Frequency.c
@@ -1,35 +1,41 @@
 #include <stdio.h>
+
 int main()
 {
-	int n,i,j;
+	int n, i, j;
+
 	printf("Size of array: ");
-	scanf("%d",&n);
-	int a[n],f[n];
-	for(i=0;i<n;i++)
+	scanf("%d", &n);
+
+	int a[n], f[n];
+	for (i = 0; i < n; i++)
 	{
-		f[i]=0;
+		f[i] = 0;
 	}
 	printf("Elements:\n");
-	for(i=0;i<n;i++)
+	for (i = 0; i < n; i++)
 	{
-		scanf("%d",&a[i]);
+		scanf("%d", &a[i]);
 	}
-	for(i=0;i<n-1;i++)
+
+	for (i = 0; i < n - 1; i++)
 	{
-		int c=1;
-		if(f[i]==1)
+		int c = 1;
+		if (f[i] == 1)
 		{
 			continue;
 		}
-		for(j=i+1;j<n;j++)
+		for (j = i + 1; j < n; j++)
 		{
-			if(a[i]==a[j])
+			if (a[i] == a[j])
 			{
-				f[j]=1;
+				f[j] = 1;
 				c++;
 			}
 		}
-		printf("\%d count: %d\n",a[i],c);
+
+		printf("%d count: %d\n", a[i], c);
 	}
+
 	return 0;
 }
